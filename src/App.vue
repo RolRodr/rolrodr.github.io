9be886f992c0f833622869df8fd578b6e1e613f3
rolrodr <rol.o.rodr@gmail.com>
2025-05-28 23:43:10 -0400

    built new site

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import TopBanner from './components/TopBanner.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <!-- <img alt="Rolando Rodriguez" class="logo" src="./assets/rolando.jpg" /> -->
+    <div class="wrapper">
+      <TopBanner msg="Rolando Rodriguez" />
+    </div>
+  </header>
+
+  <main>
+    <hr/>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+    display: flex;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+    max-width: 150px;
+    max-height: 150px;
+    border-radius: 10px;
+    
+
+
+  }
+
+}
+</style>
---
+++ src/components/TopBanner.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="bannerWrapper">  
+    <div class="greetings">
+      <h1 class="green">{{ msg }}</h1>
+      <h3>
+        Data librarian, <br>
+        Software developer, <br>
+        but mainly a people person. <br>
+      </h3>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bannerWrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+}
+
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+  margin-left: 1rem;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
